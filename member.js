d286f11833f2cccc3df3367fe50402a11c274780
Yexiong Lin <48252117+YexiongLin@users.noreply.github.com>
2023-12-30 07:08:28 +0000

    change member.js

--- member.js
+++ member.js
@@ -1,10 +1,11 @@
 function skillsMember() {
   return {
     restrict: 'E',
+    templateUrl: 'templates/skills-member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'skillsMemberCtrl',
     scope: {
-      member: '=',
-      title: '@'
-    },
-    templateUrl: 'components/member.html'
+      member: '='
+    }
   };
 }
